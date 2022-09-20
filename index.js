14152599788854aafe8e3a95aebf7996bb35535b
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-09-20 17:53:08 +0300

    minor fix

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export * from "./services/contractService";
+import {
+  PROTOCOL_CONTRACTS,
+  metaversesJson,
+  NFTMarketplace_ABI,
+  ERC20_ABI,
+  Base_ABI,
+  getRequestedNFTs,
+  getListedNFTs,
+} from "./services/contractService";
