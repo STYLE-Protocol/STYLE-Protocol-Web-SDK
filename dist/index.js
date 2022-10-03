04adae77b9bbea4ec4837c84b602f5f881acb1c0
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-10-03 11:01:12 +0300

    fix exports

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,7 @@ exports.getRequestedNFTs =
   require("../dist/services/contractService").getRequestedNFTs;
 exports.getListedNFTs =
   require("../dist/services/contractService").getListedNFTs;
+exports.approveERC20 = require("../dist/services/contractService").approveERC20;
+exports.buyItem = require("../dist/services/contractService").buyItem;
+exports.buyAndMintItem =
+  require("../dist/services/contractService").buyAndMintItem;
