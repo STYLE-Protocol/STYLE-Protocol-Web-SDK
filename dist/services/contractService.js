f08ed06f96c8061be5728248dfde20ec332f9622
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-10-21 15:42:14 +0300

    update contract

--- dist/services/contractService.js
+++ dist/services/contractService.js
@@ -9,7 +9,7 @@ const Base_metadata = require("../../public/contracts/Base_metadata.json");
 const PROTOCOL_CONTRACTS = {
   4: "0x36ACbdcBf366558AD8c6Be12F217Dc64f78d7B72",
   80001: "0xFfe8B49e11883De88e110604DA018572b93f9f24",
-  5: "0x469d74Af73694A3CC8d8573A1534E942040f1d82",
+  5: "0x80cdE9aD3355c572778070D5d60CEfef04B227f1",
 };
 
 const metaversesJson = [
