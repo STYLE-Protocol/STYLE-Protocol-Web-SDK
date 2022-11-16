bb164d253cb098baa273d4706b33f0d2494a59bc
EDU/a.korokhov <a.korokhov@innopolis.university>
2022-11-16 10:06:55 +0300

    minor fix

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 exports.getRequestedNFTs =
   require("../dist/services/contractService").getRequestedNFTs;
-exports.getListedNFTs =
-  require("../dist/services/contractService").getListedNFTs;
+exports.getRequestedSingularNFTs =
+  require("../dist/services/contractService").getRequestedSingularNFTs;
 exports.approveERC20 = require("../dist/services/contractService").approveERC20;
 exports.buyItem = require("../dist/services/contractService").buyItem;
 exports.buyAndMintItem =
