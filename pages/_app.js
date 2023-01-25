e248e6b27a89d04f9b88b57faab81ddec1f9cb35
EDU/a.korokhov <a.korokhov@innopolis.university>
2023-01-25 15:39:54 +0300

    fix theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
 import { AppProvider } from "../contexts/AppContext";
-import "../styles/globals.css";
-import theme from "../theme";
+import styles from "../styles/globals.css";
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+const customTheme = extendTheme({ config, styles });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <AppProvider>
-        <Component {...pageProps} />
-      </AppProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={customTheme}>
+        <AppProvider>
+          <Component {...pageProps} />
+        </AppProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
