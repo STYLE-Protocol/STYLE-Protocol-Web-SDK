a2ecfa6e3a8dc4dcdca3c2500f19a02ffc3702da
EDU/a.korokhov <a.korokhov@innopolis.university>
2023-01-25 14:06:39 +0300

    one file modifier

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-exports.getRequestedNFTs =
-  require("../dist/services/contractService").getRequestedNFTs;
-exports.getRequestedSingularNFTs =
-  require("../dist/services/contractService").getRequestedSingularNFTs;
-exports.approveERC20 = require("../dist/services/contractService").approveERC20;
-exports.buyItem = require("../dist/services/contractService").buyItem;
-exports.buyAndMintItem =
-  require("../dist/services/contractService").buyAndMintItem;
